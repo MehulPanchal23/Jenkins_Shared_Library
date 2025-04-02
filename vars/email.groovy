0f84b5f98105511fc8211a48c230e5c1fc7c051a
Mehul Panchal <panchalmehul195@gmail.com>
2025-04-02 21:38:22 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,16 +1,6 @@
 def call(String fromname, String toname){
-  success {
         emailext from: "${fromname}",
                  subject: '$DEFAULT_SUBJECT',
                  body: '$DEFAULT_CONTENT',
                  to: "${toname}"
-            
-        }
-    failure {
-        emailext from: "${fromname}",
-                 subject: '$DEFAULT_SUBJECT',
-                 body: '$DEFAULT_CONTENT',
-                 to: "${toname}"
-            
-    }
 }
